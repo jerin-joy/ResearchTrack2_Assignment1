a8b639cce9618474f9c5e1e2100e885062c37360
jerin-joy <jerinmemana@gmail.com>
2022-05-26 18:09:16 +0200

    write ros2 code for position_service.cpp

--- src/position_service.cpp
+++ src/position_service.cpp
@@ -1,25 +1,45 @@
-#include "ros/ros.h"
-#include "rt2_assignment1/RandomPosition.h"
+#include <inttypes.h>
+#include <memory>
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_components/register_node_macro.hpp"
+#include "rt2_assignment1/srv/random_position.hpp"
 
-//random number generation
-double randMToN(double M, double N)
-{     return M + (rand() / ( RAND_MAX / (N-M) ) ) ; }
+using RandomPositionService = rt2_assignment1::srv::RandomPosition;
+using std::placeholders::_1;
+using std::placeholders::_2;
+using std::placeholders::_3;
 
-
-bool myrandom (rt2_assignment1::RandomPosition::Request &req, rt2_assignment1::RandomPosition::Response &res){
-    res.x = randMToN(req.x_min, req.x_max);
-    res.y = randMToN(req.y_min, req.y_max);
-    res.theta = randMToN(-3.14, 3.14);
-    return true;
-}
-
-
-int main(int argc, char **argv)
+namespace rt2_assignment1
 {
-   ros::init(argc, argv, "random_position_server");
-   ros::NodeHandle n;
-   ros::ServiceServer service= n.advertiseService("/position_server", myrandom);
-   ros::spin();
 
-   return 0;
+    class RandomPosition : public rclcpp::Node
+    {
+    public:
+        RandomPosition(const rclcpp::NodeOptions & options)
+            : Node("random_position_node", options)
+        {
+            service_ = this->create_service<RandomPositionService>(
+                "/position_server", std::bind(&RandomPosition::myrandom, this, _1, _2, _3));
+        }
+
+    private:
+        double randMToN(double M, double N)
+        {
+            return M + (rand() / (RAND_MAX / (N - M)));
+        }
+
+        bool myrandom(const std::shared_ptr<rmw_request_id_t> request_header,
+                      const std::shared_ptr<RandomPositionService::Request> req,
+                      const std::shared_ptr<RandomPositionService::Response> res)
+        {
+            (void)request_header;
+            res->x = randMToN(req->x_min, req->x_max);
+            res->y = randMToN(req->y_min, req->y_max);
+            res->theta = randMToN(-3.14, 3.14);
+            return true;
+        }
+        rclcpp::Service<RandomPositionService>::SharedPtr service_;
+    };
 }
+
+RCLCPP_COMPONENTS_REGISTER_NODE(rt2_assignment1::RandomPosition)
